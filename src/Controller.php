15a151ea502c515f55d1de3084fe5e78164c7bbf
ruben <rubengorodri@gmail.com>
2021-11-26 17:12:32 +0100

    backup commit

---
+++ src/Controller.php
@@ -0,0 +1,16 @@
+<?php
+
+    namespace App;
+
+    use App\Request;
+    use App\Session;
+
+    class Controller{
+        protected $request;
+        protected $session;
+
+        function __construct(Request $request, Session $session){
+            $this->request = $request;
+            $this->session = $session;
+        }
+    }
---
+++ src/Controllers/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+
+    namespace App\Controllers;
+    
+    use App\Controller;
+    use App\Registry;
+    use App\Request;
+    use App\Session;
+
+class IndexController extends Controller{
+
+        public function __construct(Request $request, Session $session){
+            parent::__construct($request, $session);
+        }
+
+        public function index()
+        {
+            $roles = Registry::get('database')->selectAll('roles');
+             
+            return view('index', compact('roles'));
+        }
+    }
